0f0c930c6f2ed95590cf7fc08f234f10e4fc64d5
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-02-13 04:27:21 -0500

    Added point distribution graph

--- dashboard.py
+++ dashboard.py
@@ -65,8 +65,20 @@ app.layout = (
                Output(component_id='plot3', component_property='children')],
                [Input(component_id='season', component_property='value'),
                 Input(component_id='team_select', component_property='value')])
-def display_stats(input_season, input_teams):
-    return [html.P("testA"), html.P("testB"), html.P("testC")]
+def display_stats(input_season, input_team):
+    # create dataframe matching selected settings
+    current_df = df[df["team"]==input_team]
+    # fig1 age distribution
+    min_age=df["age"].min()
+    max_age=df["age"].max()
+    fig1 = px.histogram(current_df.sort_values("age"), x="age", range_x=[min_age - 1, max_age + 1],
+             title="Age Distribution",
+             color="age", color_discrete_sequence=px.colors.sequential.Plotly3)
+    fig1.update_layout(xaxis_title="age (in years)", yaxis_title="number of players", showlegend=False)
+    # fig2 points distribution
+    # fig3 penalty minutes
+    # return graphs
+    return [dcc.Graph(figure=fig1), html.P("testB"), html.P("testC")]
 
 if __name__ == '__main__':
     app.run_server()

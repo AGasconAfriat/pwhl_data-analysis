a922b915e93a8981f6af9430a5aaea1accfc40ea
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-03-16 01:11:35 -0400

    Issue fix
    Issue was fixed only for df, now fixing it for det_df

--- step1_get_stats.py
+++ step1_get_stats.py
@@ -63,7 +63,7 @@ def scrape_page_list(url_code, n, n_seasons=0): # any part of an url meant to be
     return temp_df
 # ----- WEBSCRAPING ----- ----- ----- ----- ----- ----- -----
 df = scrape_page_list("https://www.thepwhl.com/en/stats/player-stats/all-teams/5?sort=points&playertype=skater&position=skaters&rookie=no&statstype=expanded&page=<index>&league=1", 8, number_of_seasons)
-det_df = scrape_page_list("https://www.thepwhl.com/en/stats/roster/<index>/5?league=1", 6, 1) # details pages only list the current members of each team
+det_df = scrape_page_list("https://www.thepwhl.com/en/stats/roster/<index>/5?league=1", 6, 0) # details pages only list the current members of each team
 print("Webscraping complete.") #TODO remove once done with webscraping tests
 
 # ----- DATA WRANGLING ----- ----- ----- ----- ----- ----- -----
